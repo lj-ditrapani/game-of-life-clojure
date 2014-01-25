ad9f1049aa5189a2cdd521472127b38c04116212
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-01-25 12:41:42 -0500

    Neighbors test

--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -13,9 +13,17 @@
                              [nil :on]]),
          1 4 #{[0 2]} [[nil nil :on nil]])
   (testing "neighbors"
-    (is (= (neighbors 4 4 [1 1]) [[0 0] [0 1] [0 2]
-                                  [1 0]       [1 2]
-                                  [2 0] [2 1] [2 2]]))))
+    (are [height width cell neighbor-cells]
+         (= (neighbors height width cell) neighbor-cells)
+         4 4 [1 1] [[0 0] [0 1] [0 2]
+                    [1 0]       [1 2]
+                    [2 0] [2 1] [2 2]],
+         5 5 [0 0] [[4 4] [4 0] [4 1]
+                    [0 4]       [0 1]
+                    [1 4] [1 0] [1 1]],
+         4 5 [3 4] [[2 3] [2 4] [2 0]
+                    [3 3]       [3 0]
+                    [0 3] [0 4] [0 0]])))
   
 
 
